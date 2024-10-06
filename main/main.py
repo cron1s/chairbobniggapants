f59f9ff3cb5ac8124bd1446b9a4e9c13351b9a64
TheCrazy0n3 <zingerle.josua@gmail.com>
2024-10-06 18:20:38 +0200

    keys file

--- main/main.py
+++ main/main.py
@@ -1,3 +1,11 @@
 from lib.bot import bot
+import json
+
+openAiKey = str()
+discordKey = str()
+with open('keys.json') as file:
+    json = json.load(file)
+    openAiKey = json["openAiKey"]
+    discordKey = json["discordKey"]
 
 bot.run()
