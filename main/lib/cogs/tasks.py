df4a069dd1af13aeddc6570b43047289da91aa0a
TheCrazy0n3 <95098224+TheCrazy0n3@users.noreply.github.com>
2024-10-07 17:21:56 +0200

    fixed error in tasks

--- main/lib/cogs/tasks.py
+++ main/lib/cogs/tasks.py
@@ -1,8 +1,13 @@
+import discord
 from discord.ext import tasks, commands
 
 class Tasks(commands.Cog):
-     def __init__(self, bot):
-          self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
+
+    @tasks.loop(hours=2.0)
+    async def change_presence(self):
+        await self.bot.change_presence(type=discord.ActivityType.watching, name="in do saily ban gaggn zui")
 
 async def setup(bot):
     await bot.add_cog(Tasks(bot))
